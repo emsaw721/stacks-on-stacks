e320bdb49a2f3097e8f3febf21851f9695a7703d
Emily Hill <emhillmed2021@gmail.com>
2023-01-17 15:48:39 -0600

    threw something into App.js, still need to do the inside portion

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,27 @@
-import React from 'react'; 
+import React  from 'react'; 
+import {Link} from 'react-router-dom';
 import './pages.css'; 
 import Navpig from 'oinktranspig.png'; 
 
-function Nav({currectPage, handlePageChange}) {
+function Nav() {
     return(
-        
+        <nav className='navbar' >
+            <ul className='nav-list'>
+                <li className='nav-item'>
+                    <Link to '/Expense' className='nav-link'>
+                        <span className='link-text'>Expense</span>
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to '/Income' className='nav-link'>
+                        <span className='link-text'>Income</span>
+                    </Link>
+                </li>
+                <li className='dash-icon'>
+                    <Link to '/Dashboard' className='nav-link'>
+                        <img src={Navpig} />
+                    </Link>
+                </li>
     )
 }
 
