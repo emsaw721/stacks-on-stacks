48b8d0ac59a8d6407fe907872a8b46b2a0dcbb8a
TB <tatianabertazoli@gmail.com>
2023-01-18 17:23:56 -0600

    commiting so I can pull

--- client/src/pages/Planner.js
+++ client/src/pages/Planner.js
@@ -4,14 +4,25 @@ import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css'
 
 const Planner = () => {
-    const [value, onChange] = useState(new Date());
-  
+    // set states of calendar date
+    const [calDate, setCalDate] = useState(new Date())
+
+    function onChange(calDate) {
+        setCalDate(calDate)
+
+    }
+
+// function to open calendar on a given day and view expenses and or income
+    
+
+
     return (
-      <div className="calendar">
-        <Calendar onChange={onChange} value={value} />
-      </div>
-    );
-}  
+        <div className="calendar">
+            <Calendar onChange={onChange} value={calDate} />
+        </div>
+    )
+
+}
 
 
 export default Planner;
