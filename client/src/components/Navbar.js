0f48d7072d9d54abe1c466d3fd065ce705b48db2
Emily Hill <109986121+emsaw721@users.noreply.github.com>
2023-01-17 21:57:09 -0600

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,37 +1,42 @@
 import React  from 'react'; 
 import {Link} from 'react-router-dom';
+// import Expense from './pages/Expense';
+// import Income from './pages/Income';
+// import Planner from './pages/Planner';
+// import Dashboard from './pages/Dashboard'; 
+// import Signout from './pages/Signout'; 
 import Auth from '../utils/auth';
 import './components.css'; 
-import Navpig from 'oinktranspig.png'; 
+import Navpig from './oinktranspig.png'; 
 
 const Nav = () => {
     return(
         <nav className='navbar' >
             <ul className='nav-list'>
                 <li className='nav-item'>
-                    <Link to '/Expense' className='nav-link'>
+                    {/* <Link to {...Expense} className='nav-link'> */}
                         <span className='link-text'>Expense</span>
-                    </Link>
+                    {/* </Link> */}
                 </li>
                 <li className='nav-item'>
-                    <Link to '/Income' className='nav-link'>
+                    {/* <Link to {...Income} className='nav-link'> */}
                         <span className='link-text'>Income</span>
-                    </Link>
+                    {/* </Link> */}
                 </li>
                 <li className='dash-icon'>
-                    <Link to '/Dashboard' className='nav-link'>
+                    {/* <Link to {...Dashboard} className='nav-link'> */}
                         <img src={Navpig} />
-                    </Link>
+                    {/* </Link> */}
                 </li>
                 <li className='nav-item'>
-                    <Link to '/Planner' className='nav-link'>
+                    {/* <Link to {...Planner} className='nav-link'> */}
                         <span className='link-text'>Budget Planner</span>
-                    </Link>
+                    {/* </Link> */}
                 </li>
                 <li className='nav-item'>
-                    <Link to '/Signout' className='nav-link'>
+                    {/* <Link to {...Signout} className='nav-link'> */}
                         <span className='link-text'>Signout</span>
-                    </Link>
+                    {/* </Link> */}
                 </li>
             </ul>
         </nav>
