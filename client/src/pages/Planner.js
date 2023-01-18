e0fc1bf521b7f9a23c5cac461fc891addb9b4122
TB <tatianabertazoli@gmail.com>
2023-01-17 21:33:53 -0600

    added styling to the calendar

--- client/src/pages/Planner.js
+++ client/src/pages/Planner.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import './pages.css';
 import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css'
 
 const Planner = () => {
     const [value, onChange] = useState(new Date());
   
     return (
-      <div>
+      <div className="calendar">
         <Calendar onChange={onChange} value={value} />
       </div>
     );
