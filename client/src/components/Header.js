8e708827520d21e69553ba2656afacdbbf852e79
Emily Hill <emhillmed2021@gmail.com>
2023-01-17 21:07:12 -0600

    clearing out work to see calendar

---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Header = () => {
+
+    return (
+        <div className='header'>
+            <h1>This is the header, maybe?</h1>
+        </div>
+    )
+}
+
+export default Header; 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,43 +1,43 @@
 import React  from 'react'; 
 import {Link} from 'react-router-dom';
-import Expense from './pages/Expense';
-import Income from './pages/Income';
-import Planner from './pages/Planner';
-import Dashboard from './pages/Dashboard'; 
-import Signout from './pages/Signout'; 
+// import Expense from './pages/Expense';
+// import Income from './pages/Income';
+// import Planner from './pages/Planner';
+// import Dashboard from './pages/Dashboard'; 
+// import Signout from './pages/Signout'; 
 import Auth from '../utils/auth';
 import './components.css'; 
-import Navpig from 'oinktranspig.png'; 
+import Navpig from './oinktranspig.png'; 
 
 const Nav = () => {
     return(
         <nav className='navbar' >
             <ul className='nav-list'>
-                <li className='nav-item'>
-                    <Link to {Expense} className='nav-link'>
+                {/* <li className='nav-item'>
+                    <Link to {...Expense} className='nav-link'>
                         <span className='link-text'>Expense</span>
                     </Link>
                 </li>
                 <li className='nav-item'>
-                    <Link to {Income} className='nav-link'>
+                    <Link to {...Income} className='nav-link'>
                         <span className='link-text'>Income</span>
                     </Link>
                 </li>
                 <li className='dash-icon'>
-                    <Link to {Dashboard} className='nav-link'>
+                    <Link to {...Dashboard} className='nav-link'>
                         <img src={Navpig} />
                     </Link>
-                </li>
-                <li className='nav-item'>
-                    <Link to {Planner} className='nav-link'>
+                </li> */}
+                {/* <li className='nav-item'>
+                    <Link to {...Planner} className='nav-link'>
                         <span className='link-text'>Budget Planner</span>
                     </Link>
                 </li>
                 <li className='nav-item'>
-                    <Link to {Signout} className='nav-link'>
+                    <Link to {...Signout} className='nav-link'>
                         <span className='link-text'>Signout</span>
                     </Link>
-                </li>
+                </li> */}
             </ul>
         </nav>
     )
