6ffe2482d6acd8cd5b0ff964f7e9f4d53fe23b0b
Emily Hill <109986121+emsaw721@users.noreply.github.com>
2023-01-17 21:57:33 -0600

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,1 +1,21 @@
+import React from 'react';
+import './components.css'; 
+import Auth from '../utils/auth';
 
+const Header = () => {
+
+    return (
+    <section className='header'>
+        <h1>This is the header!</h1>
+            {/* {Auth.loggedin() ? (
+                <div className='header'>
+                <h1> Hello, Username! </h1>
+                </div>
+            ):(  <div className='header'>
+            <h1>This is the header, maybe?</h1>
+            </div>)} */}
+    </section>
+    )
+}
+
+export default Header; 
