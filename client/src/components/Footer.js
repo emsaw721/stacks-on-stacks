59e505ce49128c2f4cc4d4d38d66257061f01918
Emily Hill <109986121+emsaw721@users.noreply.github.com>
2023-01-17 21:57:57 -0600

    Update Footer.js

---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './components.css';
+
+const Footer = () => {
+
+    return (
+        <div className='footer'>
+            <h3>Some Footer Text and Color</h3>
+        </div>
+    )
+}
+
+export default Footer; 
