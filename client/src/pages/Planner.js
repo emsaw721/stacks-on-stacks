f51d612eb5c1b7f5172021ecc29b5617737e206d
Emily Hill <109986121+emsaw721@users.noreply.github.com>
2023-01-17 22:15:17 -0600

    Update Planner.js

--- client/src/pages/Planner.js
+++ client/src/pages/Planner.js
@@ -1,22 +1,34 @@
-import React from 'react';
-import './components.css'; 
-import Auth from '../utils/auth';
 
-const Header = () => {
+import React from 'react';
+import {Doughnut} from 'react-chartjs-2';
+import './pages.css';
+
+const Planner = () => {
 
     return (
-    <section className='header'>
-        <h1>This is the header!</h1>
-            {/* {Auth.loggedin() ? (
-                <div className='header'>
-                <h1> Hello, Username! </h1>
-                </div>
-            ):(  <div className='header'>
-            <h1>This is the header, maybe?</h1>
-            </div>)} */}
+        <section className='planner'>
+        <h1> You Budget Planner </h1>
+    <Doughnut data={{
+        datasets: [
+            {
+                id: 1,
+                label: 'Expense1',
+                data: [5,6,7],
+            },
+            {
+                id: 2,
+                label: 'Expense2',
+                data: [3,2,1]
+            },
+            {
+                id: 3, 
+                label: 'Expense3',
+                data: [7,8,9]
+            },
+        ],
+    }}
+    />
     </section>
     )
 
 }
-
-export default Header; 
