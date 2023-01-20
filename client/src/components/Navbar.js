59e33b4843fa160a19aeacc2f65e306ca1ec9ace
TB <tatianabertazoli@gmail.com>
2023-01-19 20:14:58 -0600

    adding login/logout on

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,45 +1,93 @@
-import React  from 'react'; 
-import {Link} from 'react-router-dom';
-import './components.css'; 
-import Navpig from './oinktranspig.png'; 
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Modal, Tab, Nav } from 'react-bootstrap'
+import './components.css';
+import Navpig from './oinktranspig.png';
+import LoginForm from './LoginForm';
+import SignUpForm from './SignupForm'
 import Auth from '../utils/auth';
 
-const Nav = () => {
-    return(
+// import Expense from './pages/Expense';
+// import Income from './pages/Income';
+// import Planner from './pages/Planner';
+// import Dashboard from './pages/Dashboard'; 
+
+
+
+const AppNav = () => {
+    const [showModal, setShowModal] = useState(false);
+
+    return (
         // {Auth.loggedin() ? (
         <section className='navcontainer'>
-        <nav className='navbar' >
-            <ul className='nav-list'>
-                <li className='nav-item'>
-                    {/* <Link to '/expense' className='nav-link'> */}
+            <nav className='navbar' >
+                <ul className='nav-list'>
+                    <li className='nav-item'>
+                        {/* <Link to '/expense' className='nav-link'> */}
                         <span className='link-text'>Expense</span>
-                    {/* </Link> */}
-                </li>
-                <li className='nav-item'>
-                    {/* <Link to '/income' className='nav-link'> */}
+                        {/* </Link> */}
+                    </li>
+                    <li className='nav-item'>
+                        {/* <Link to '/income' className='nav-link'> */}
                         <span className='link-text'>Income</span>
-                    {/* </Link> */}
-                </li>
-                <li className='dash-icon'>
-                    {/* <Link to '/dashboard' className='nav-link'> */}
+                        {/* </Link> */}
+                    </li>
+                    <li className='dash-icon'>
+                        {/* <Link to '/dashboard' className='nav-link'> */}
                         <img src={Navpig} />
-                    {/* </Link> */}
-                </li>
-                <li className='nav-item'>
-                    <Link to='/planner' className='nav-link'>
-                        <span className='link-text'>Report</span>
-                    </Link>
-                </li>
-                <li className='nav-item'>
-                    <Link onClick={Auth.logout}>
-                        <span className='link-text'>Signout</span>
-                    </Link>
-                </li>
-            </ul>
-        </nav>
+                        {/* </Link> */}
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/planner' className='nav-link'>
+                            <span className='link-text'>Planner</span>
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link onClick={Auth.logout}>
+                            <span className='link-text'>Signout</span>
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Nav.Link onClick={() => setShowModal(true)}>
+                            Login/Signup
+                        </Nav.Link>
+                    </li>
+                </ul>
+            </nav>
+            {/* set modal data up */}
+            <Modal
+                size='lg'
+                show={showModal}
+                onHide={() => setShowModal(false)}
+                aria-labelledby='signup-modal'>
+                {/* tab container to do either signup or login component */}
+                <Tab.Container defaultActiveKey='login'>
+                    <Modal.Header closeButton>
+                        <Modal.Title id='signup-modal'>
+                            <Nav variant='pills'>
+                                <Nav.Item>
+                                    <Nav.Link eventKey='login'>Login</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Tab.Content>
+                            <Tab.Pane eventKey='login'>
+                                <LoginForm handleModalClose={() => setShowModal(false)} />
+                            </Tab.Pane>
+                            <Tab.Pane eventKey='signup'>
+                                <SignUpForm handleModalClose={() => setShowModal(false)} />
+                            </Tab.Pane>
+                        </Tab.Content>
+                    </Modal.Body>
+                </Tab.Container>
+            </Modal>
         </section>
-        // ):()};
     )
 };
 
-export default Nav; 
+export default AppNav; 
