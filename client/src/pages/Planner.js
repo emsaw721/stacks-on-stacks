160bdf4f7354ceaa1a7821beb1daa9a13fdeac66
Emily Hill <emhillmed2021@gmail.com>
2023-01-19 20:47:45 -0600

    making more progress toward finishing Report page, need to finish query budget data for each hard coded category, save the user input, and then finish the select a category option

--- client/src/pages/Planner.js
+++ client/src/pages/Planner.js
@@ -1,19 +1,39 @@
-import React from 'react'
+import React, {useState, useAPI} from 'react';
+import {useQuery} from '@apollo/client';
+import {QUERY_BUDGET} from '../utils/queries';
 import './pages.css';
 
 const Planner = () => {
+//    const {data, isLoading} = useAPI(); 
+//    const [selectedCategory, setSelectedCategory] = useState(''); 
+//    const filterCategories = data.filter(budget => budget.category === selectedCategory);
 
     return (
         <section className='planner'>
             <h1> Your Progress Report </h1>
             <table>
                 <tr>
+                    <th>Category</th>
                     <th>Expected</th>
                     <th>Actual</th>
                 </tr>
                 <tr>
-                    <td></td>
+                    <td>Rent</td>
+                    <td><input></input></td>
                 </tr>
+                <tr>
+                    <td>Utilities</td>
+                    <td><input></input></td>
+                </tr>
+                <tr>
+                    <td>Grocery</td>
+                    <td><input></input></td>
+                </tr>
+                <tr>
+                    <td>Other</td>
+                    <td><input></input></td>
+                </tr>
+                {/* <tr><select value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)} name='category-dropdown'></select></tr> */}
             </table>
         </section>
     )
