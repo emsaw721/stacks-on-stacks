dcbaabbeb04bb0a0d3a53f31d8d8864a719b3260
Emily Hill <emhillmed2021@gmail.com>
2023-01-19 20:10:45 -0600

    started table for budget progress report page

--- client/src/pages/Planner.js
+++ client/src/pages/Planner.js
@@ -6,7 +6,15 @@ const Planner = () => {
     return (
         <section className='planner'>
             <h1> Your Progress Report </h1>
-            
+            <table>
+                <tr>
+                    <th>Expected</th>
+                    <th>Actual</th>
+                </tr>
+                <tr>
+                    <td></td>
+                </tr>
+            </table>
         </section>
     )
 }
