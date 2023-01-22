c29a404c953810180a79a206093d552b950e119a
Patrick-Duffy202 <Patrickduffy202@gmail.com>
2023-01-22 06:22:51 -0600

    expense sheet

--- client/src/App.js
+++ client/src/App.js
@@ -20,15 +20,6 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -8,26 +8,6 @@ function Login(props) {
   const [formState, setFormState] = useState({ email: '', password: '' });
   const [login, { error }] = useMutation(LOGIN);
 
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const mutationResponse = await login({
-        variables: { email: formState.email, password: formState.password },
-      });
-      const token = mutationResponse.data.login.token;
-      AuthService.login(token);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
-  };
 
   return (
     <div className="container my-1">
