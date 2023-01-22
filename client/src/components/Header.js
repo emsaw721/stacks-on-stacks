28d9c5b9c6f44ec5629971fac77fb16ea71c6290
TB <tatianabertazoli@gmail.com>
2023-01-21 18:14:23 -0600

    adde logic to username in the header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import './components.css'; 
+import './components.css';
 import Auth from '../utils/auth';
 import Oinkicon from './oinkicon1.png';
 import Welcomeicon from './welcomepig.png';
+import decode from 'jwt-decode';
+
+
 const Header = () => {
 
+    let username = ""
+    const token = localStorage.getItem('id_token')
+    
+    if (token) {
+        const decoded = decode(token); 
+       username = decoded.data.username
+    }
+
     return (
-    <section className='header'>
-        {Auth.loggedIn() ? (
-        <div className='loggedin'>
-        <h1>Hello, Username!</h1>
-        <img src={Oinkicon} alt=""/> 
-        </div>
-        ):(
-            <div className='loggedout'>
-            <h1>Welcome to Oink!</h1>
-            <img src={Welcomeicon} alt=""/>
-            </div>
+        <section className='header'>
+            {Auth.loggedIn() ? (
+                <div className='loggedin'>
+                    <h1>Hello, { username}!</h1>
+                    <img src={Oinkicon} alt="" />
+                </div>
+            ) : (
+                <div className='loggedout'>
+                    <h1>Welcome to Oink!</h1>
+                    <img src={Welcomeicon} alt="" />
+                </div>
             )}
-    </section>
+        </section>
     )
 }
 
