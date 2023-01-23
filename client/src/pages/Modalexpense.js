f682420acabf9460aa89db87abd1258846ebdd74
TB <tatianabertazoli@gmail.com>
2023-01-23 15:53:43 -0600

    added the expense form to a modal

--- client/src/pages/Modalexpense.js
+++ client/src/pages/Modalexpense.js
@@ -1,46 +1,45 @@
 import React, { useState } from 'react';
-import './pages.css';
-import { Button } from 'react-bootstrap'
-import Modalexpense from '../components/Modalexpense';
-
-
-
-const Expense = (data) => {
-
-    const [isModalOpen, setIsModalOpen] = useState(false);
-
-    const openLink = () => {        
-        setIsModalOpen(!isModalOpen);
-    }
-
-    return (
-        <section className="flex-row justify-center mb-4">
-            <div className="col-12 col-md-6">
-                <div className="card">
-                    <h4 className="card-header">Expense List</h4>
-                    <div className="card-body">
-                        <Modalexpense show={isModalOpen} onClose={setIsModalOpen}></Modalexpense>
-                        <Button type='button' variant='success' className='subbtn' onClick={openLink}>
-                            Add new expense
-                        </Button>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
-};
-
-
-import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
 import './pages.css';
 import { Form, Button } from 'react-bootstrap'
+// import { Link } from 'react-router-dom';
+import { ADD_TRANSACTION } from '../utils/mutations';
 import Auth from '../utils/auth';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 
 
+const Modalexpense = (props) => {
 
-const Expense = (props) => {
+  const [validated] = useState(false);  
 
+  const [expenseFormState, setExpenseState] = useState({
+    firstCategory: 'Expense',
+    secondCategory: '',
+    amount: '',
+    categoryNote: ''
+  });
 
+  const [addExpense] = useMutation(ADD_TRANSACTION);
+
+  const handleInputChange = (event) => {
+    const { firstCategory, secondCategory, amount, categoryNote, value } = event.target;
+    setExpenseState({ ...expenseFormState, [firstCategory]: value, [secondCategory]: value, [amount]: value, [categoryNote]: value });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await addExpense({
+        variables: { ...expenseFormState },
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   const secondCategoryDropdown = ['Housing', 'Utility', 'Food', 'Transportation', 'Insurance', 'Education', 'Healthcare', 'Savings & Investiment', 'Personal spending', 'Others'];
 
@@ -48,7 +47,7 @@ const Expense = (props) => {
     <section className="flex-row justify-center mb-4">    
       <div className="col-12 col-md-6">
         <div className="card">
-          <h4 className="card-header">Expense List</h4>
+          <h4 className="card-header">Add Your Expenses</h4>
           <div className="card-body">
             <Form onSubmit={handleFormSubmit} noValidate validated={validated}>            
               <Dropdown options={secondCategoryDropdown} onChange={handleInputChange} value={expenseFormState.secondCategory} placeholder="Select an option" className='form-input'></Dropdown>
@@ -73,7 +72,7 @@ const Expense = (props) => {
               <Button
                 disabled={!(expenseFormState.firstcategory && expenseFormState.secondcategory)}
                 type='submit' variant='success' className='subbtn'>
-                Add new expense
+                Submit
               </Button>
             </Form>
           </div>
@@ -84,4 +83,5 @@ const Expense = (props) => {
 };
 
 
-export default Expense;
+
+export default Modalexpense;
