6e0fe47bb5c18262e684e3963089c127c5b5595a
Emily Hill <emhillmed2021@gmail.com>
2023-01-24 13:50:34 -0600

    Fixing login SignupForm

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -25,95 +25,25 @@ const SignupForm = () => {
     event.preventDefault();
 
     // check if form has everything (as per react-bootstrap docs)
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-
-    // check if form has everything (as per react-bootstrap docs)
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
+    const loginForm = event.currentTarget;
+    if (loginForm.checkValidity() === false) {
       event.preventDefault();
       event.stopPropagation();
     }
 
     try {
       const {data} = await createUser({
-      const {data} = await createUser({
         variables: { ...formState },
       }); 
-
-   
       Auth.login(JSON.stringify(data.createUser));
     } catch (error) {
       console.error(error);
       setShowAlert(true);
-      }); 
+      }; 
 
-   
-      Auth.login(JSON.stringify(data.createUser));
-    } catch (error) {
-      console.error(error);
-      setShowAlert(true);
-    }
   };
 
   return (
-    <>
-    <style>
-  @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed:wght@100;300;400&family=IBM+Plex+Sans:wght@100&display=swap');
-</style>
-      {/* This is needed for the validation functionality above */}
-      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-        {/* show alert if server response is bad */}
-        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
-          Something went wrong with your signup!
-        </Alert>
-
-        <Form.Group>
-          <Form.Label className='formlabel' htmlFor='username'>Username</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Your username'
-            name='username'
-            onChange={handleInputChange}
-            value={formState.username}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Username is required!</Form.Control.Feedback>
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Label className='formlabel' htmlFor='email'>Email</Form.Label>
-          <Form.Control
-            type='email'
-            placeholder='Your email address'
-            name='email'
-            onChange={handleInputChange}
-            value={formState.email}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Label className='formlabel' htmlFor='password'>Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='Your password'
-            name='password'
-            onChange={handleInputChange}
-            value={formState.password}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
-        </Form.Group>
-        <Button className='formbtn'
-          disabled={!(formState.username && formState.email && formState.password)}
-          type='submit'
-          variant='success'
-          >
     <>
     <style>
   @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed:wght@100;300;400&family=IBM+Plex+Sans:wght@100&display=swap');
@@ -174,5 +104,5 @@ const SignupForm = () => {
     </>
   );
 };
-
-export default SignupForm;
+}; 
+export default SignupForm; 
