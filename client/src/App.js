16032700443dacb287678c4d1bd9a01428aef234
Emily Hill <emhillmed2021@gmail.com>
2023-01-24 12:30:43 -0600

    need to clear working branch to Tatiana can look at it without conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import Header from './components/Header';
 import Hero from './components/Hero'; 
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
-//import Expense from './pages/Expense';
+import Expense from './pages/Expense';
 import Planner from './pages/Planner';
 //import Dashboard from './pages/Dashboard'; 
 import Bar from './components/ProgressBar';
@@ -47,7 +47,7 @@ function App() {
         <div className='content'>
             <Routes>
             {/* <Route path = '/' element={<Dashboard />} />  */}
-            {/*<Route path = '/expense' element={<Expense />} /> */}
+            <Route path = '/expense' element={<Expense />} />
             <Route path='/planner' element={<Planner />} />
             </Routes>
         </div>
