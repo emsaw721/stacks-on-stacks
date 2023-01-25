326612200a32c1fca5b235e7f3e35ed5e896b486
Emily Hill <emhillmed2021@gmail.com>
2023-01-24 20:52:51 -0600

    deploy to heroku

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,20 +1,2 @@
-import React from 'react';
-import Bar from './ProgressBar'; 
-import './components.css';
-import Auth from '../utils/auth';
-
-const Hero = () => {
-    return(
-        <>
-        {Auth.loggedIn() ? (
-        <div className='hero'>
-            <Bar />
-        </div>
-        ):(
-            <div className='heronone'></div>
-        )}
-        </>
-    )
-}
 
 export default Hero; 
