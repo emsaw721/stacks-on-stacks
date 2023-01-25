b4ddb6d50d048bdd7148052cb343281318dc44f9
TB <tatianabertazoli@gmail.com>
2023-01-25 17:31:25 -0600

    fixed datepicker for income

--- client/src/components/Modalexpense.js
+++ client/src/components/Modalexpense.js
@@ -58,7 +58,7 @@ const Modalexpense = ({ show, onClose }) => {
             className="form-input"
             placeholder="Total"
             name="amount"
-            type="amount"
+            type="number"
             id="amount"
             value={expenseFormState.amount}
             onChange={handleInputChange}
@@ -67,7 +67,7 @@ const Modalexpense = ({ show, onClose }) => {
             className="form-input"
             placeholder="Notes"
             name="categoryNote"
-            type="categoryNote"
+            type="text"
             id="categoryNote"
             value={expenseFormState.categoryNote}
             onChange={handleInputChange}
